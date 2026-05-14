884309b7685f86a46e594742fa1b0295e557e337
Aditya Bhardwaj <45880118+bunnyOO7@users.noreply.github.com>
2026-05-14 11:58:05 +0530

    Update API URL and improve review loading logic

--- frontend/script.js
+++ frontend/script.js
@@ -1,6 +1,6 @@
 let currentReviewId = null;
 
-const API_URL = "http://127.0.0.1:8000";
+const API_URL = "https://review-system-1-ub1h.onrender.com";
 const GOOGLE_REVIEW_URL = "https://g.page/r/CUn49duBi3nNEBM/review";
 
 window.onload = function(){
